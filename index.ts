3e5be0f80253f0caf1e9b02fba28ff03c0584db5
Sam Vervaeck <sam@vervaeck.be>
2017-08-16 15:56:22 +0200

    Add an implementation

---
+++ index.ts
@@ -0,0 +1,53 @@
+
+import { Server, createServer } from "net"
+
+type Assignable<T> = { [P in keyof T]: T[P] } & { [key: string]: any };
+
+async function findFreePorts(count: number = 1) {
+
+
+  const servers = await Promise.all((function () {
+
+    const servers = [];
+
+    for (let i = 0; i < count; ++i) {
+      servers.push(new Promise<Server>((accept, reject) => {
+        const server = createServer();
+        server.once('listening', () => accept(server));
+        server.once('error', reject);
+        server.listen();
+      }));
+    }
+    
+    return servers;
+
+  })());
+
+  console.log('her');
+
+  return new Promise<number[]>((accept, reject) => {
+
+    function tryAccept() {
+      for (const server of servers)
+        if (!server.closed)
+          return;
+      console.log('here');
+      accept(servers.map(s => s.port));     
+    }
+
+    for (const server of servers) {
+      server.once('close', () => {
+        console.log('here');
+        server.closed = true;
+        tryAccept();
+      });
+      server.port = server.address().port;
+      server.close();          
+    }
+
+  });
+
+}
+
+export = findFreePorts;
+
