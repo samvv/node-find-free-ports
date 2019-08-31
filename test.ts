7978bfc472dc261791befa9b87386a2521634e07
Sam Vervaeck <vervaeck.sam@skynet.be>
2019-08-31 20:48:57 +0200

    Update tests

--- test.ts
+++ test.ts
@@ -1,45 +1,62 @@
 
-import { expect } from "chai"
-
-import findFreePorts = require('./');
+import findFreePorts = require('./index');
 import * as net from "net"
-// import isFree = require('./isFree');
+import * as chai from "chai"
+import * as chaiAsPromised from "chai-as-promised"
+
+chai.use(chaiAsPromised);
+
+const { expect, assert } = chai;
+
+const PORT_COUNT = 28;
 
 describe('a free port finder', () => {
-  
-  it('can find some free ports', done => {
 
-    findFreePorts(10000).then(ports => {
+  it('crashes properly when there are not enough free ports', async () => {
+    expect(findFreePorts(10, { startPort: 65530 })).to.eventually.be.rejectedWith(Error);
+  })
 
-      const listening = new Set<net.Server>();
+  it('can find some free ports', async () => {
+
+    for (let i = 0; i < 10; i++) {
+
+      const ports = await findFreePorts(PORT_COUNT);
+
+      expect(ports).to.have.lengthOf(PORT_COUNT);
+        
+      const set = new Set();
+      for (const port of ports) {
+        if (set.has(port)) {
+          assert.fail('Duplicate port found in result set.')
+        } else {
+          set.add(port);
+        }
+      }
 
       for (const port of ports) {
-        const server = net.createServer();
-        server.once('listening', () => { listening.add(server); tryClose(); })
-        server.listen(port);
+        expect(isFree(port)).to.eventually.be.true;
       }
 
-      function tryClose() {
-        if (listening.size === ports.length) {
-          for (const server of listening) {
-            server.once('close', () => {
-              listening.delete(server)
-              tryFinish();
-            })
-            server.close();
-          }
-        }
-      }
-
-      function tryFinish() {
-        if (listening.size === 0) {
-          done();
-        }
-      }
-
-    });
+    }
 
   });
 
 });
+
+
+function isFree(port: number): Promise<boolean> {
+  return new Promise((accept, reject) => {
+    const sock = net.createConnection(port);
+    sock.once('connect', () => { sock.end() });
+    sock.once('close', () => { accept(false); })
+    sock.once('error', (e: NodeJS.ErrnoException) => {
+      sock.destroy();
+      if (e.code === 'ECONNREFUSED') {
+        accept(true)
+      } else {
+        reject(e);
+      }
+    });
+  });
+}
 
