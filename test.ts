37f61d729bcddba593755d8bbde44badeb824da5
Sam Vervaeck <vervaeck.sam@skynet.be>
2019-08-31 14:26:51 +0200

    Make tests more severe

--- test.ts
+++ test.ts
@@ -2,14 +2,43 @@
 import { expect } from "chai"
 
 import findFreePorts = require('./');
-import isFree = require('./isFree');
+import * as net from "net"
+// import isFree = require('./isFree');
 
 describe('a free port finder', () => {
   
-  it('can find some free ports', async () => {
-    const ports = await findFreePorts(2);
-    for (const port of ports)
-      expect(await isFree(port)).to.be.true
+  it('can find some free ports', done => {
+
+    findFreePorts(10000).then(ports => {
+
+      const listening = new Set<net.Server>();
+
+      for (const port of ports) {
+        const server = net.createServer();
+        server.once('listening', () => { listening.add(server); tryClose(); })
+        server.listen(port);
+      }
+
+      function tryClose() {
+        if (listening.size === ports.length) {
+          for (const server of listening) {
+            server.once('close', () => {
+              listening.delete(server)
+              tryFinish();
+            })
+            server.close();
+          }
+        }
+      }
+
+      function tryFinish() {
+        if (listening.size === 0) {
+          done();
+        }
+      }
+
+    });
+
   });
 
 });
