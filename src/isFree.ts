249344880a52dd117ccd4a35e6b5e8c578bf8630
Sam Vervaeck <vervaeck.sam@skynet.be>
2020-06-11 14:08:21 +0200

    Fix isFree() hanging in some cases

--- src/isFree.ts
+++ src/isFree.ts
@@ -4,8 +4,7 @@ import * as net from "net"
 export function isFree(port: number): Promise<boolean> {
   return new Promise((accept, reject) => {
     const sock = net.createConnection(port);
-    sock.once('connect', () => { sock.end() });
-    sock.once('close', () => { accept(false); })
+    sock.once('connect', () => { sock.end(); accept(false); });
     sock.once('error', (e: NodeJS.ErrnoException) => {
       sock.destroy();
       if (e.code === 'ECONNREFUSED') {
